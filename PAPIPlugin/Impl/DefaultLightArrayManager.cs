72f8b8588aa6861e083dfe54c19ea24aa1fc9ab6
asarium <asarium@gmail.com>
2013-08-03 12:55:31 +0200

    Added GUI for PAPI arrays.

--- PAPIPlugin/Impl/DefaultLightArrayManager.cs
+++ PAPIPlugin/Impl/DefaultLightArrayManager.cs
@@ -5,6 +5,9 @@ using System.Diagnostics;
 using System.Linq;
 using PAPIPlugin.Interfaces;
 using PAPIPlugin.Internal;
+using PAPIPlugin.UI;
+using Tac;
+using UnityEngine;
 
 #endregion
 
@@ -12,7 +15,18 @@ namespace PAPIPlugin.Impl
 {
     public class DefaultLightArrayManager : ILightArrayManager
     {
+        private readonly Icon<DefaultLightArrayManager> _groupWindowIcon;
+
+        private GroupWindow<ILightArrayConfig> _groupWindow;
+
         private ILightArrayConfig _lightConfig;
+
+        public DefaultLightArrayManager()
+        {
+            _groupWindowIcon = new Icon<DefaultLightArrayManager>(new Rect(Screen.width * 0.8f, 0.0f, 80.0f, 20.0f), "icon.png", "Light groups",
+                "Opens the light group overview", OnIconClickHandler);
+            _groupWindowIcon.SetVisible(true);
+        }
 
         #region ILightArrayManager Members
 
@@ -68,6 +82,19 @@ namespace PAPIPlugin.Impl
 
         #endregion
 
+        private void OnIconClickHandler()
+        {
+            if (_groupWindow == null)
+            {
+                _groupWindow = new GroupWindow<ILightArrayConfig>(LightConfig);
+                _groupWindow.SetVisible(true);
+            }
+            else
+            {
+                _groupWindow.ToggleVisible();
+            }
+        }
+
         private void InitializeConfig(ILightArrayConfig lightConfig)
         {
             foreach (var lightArray in lightConfig.LightArrayGroups.SelectMany(group => group.LightArrays))
@@ -93,6 +120,13 @@ namespace PAPIPlugin.Impl
         protected virtual void Dispose(bool disposing)
         {
             LightConfig.Destroy();
+
+            _groupWindowIcon.SetVisible(false);
+
+            if (_groupWindow != null)
+            {
+                _groupWindow.SetVisible(false);
+            }
         }
     }
 }
--- PAPIPlugin/UI/GroupWindow.cs
+++ PAPIPlugin/UI/GroupWindow.cs
@@ -3,6 +3,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using PAPIPlugin.Interfaces;
+using PAPIPlugin.Internal;
 using Tac;
 using UnityEngine;
 
